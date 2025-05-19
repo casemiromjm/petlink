b5357a8d22172da1f565dd7ae1a15f280b58d8e4
Sara García <up202306877@up.pt>
2025-05-19 16:43:01 +0100

    Some improvements and CSS changes

--- pages/config.php
+++ pages/config.php
@@ -5,7 +5,24 @@
   require_once('../templates/sidebar.php'); 
   require_once('../templates/config.php');
 
+  session_start();
+  if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit;
+  }
+
   drawHeader();
-  drawNavbar();
+?>
+<body>
+  <div class="profile-container"> 
+    <aside class="side-nav">
+      <?php drawNavbar(); ?>
+    </aside>
+    <section class="profile-section"> 
+      <?php drawConfig(); ?>
+    </section>
+  </div>
+</body>
+<?php
   drawFooter();
 ?>
