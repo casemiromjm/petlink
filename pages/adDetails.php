952f5e0d171013de67bac2ba902a65c9fd633926
Sara García <up202306877@up.pt>
2025-05-25 16:19:24 +0100

    Moved some misplaced php code from pages to templates

--- pages/adDetails.php
+++ pages/adDetails.php
@@ -1,38 +1,15 @@
 <?php
   declare(strict_types = 1);
 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-
   require_once(__DIR__.'/../templates/layout.php');
   require_once(__DIR__.'/../templates/adDetails.php');
-  require_once(__DIR__.'/../templates/reviews.php');
   require_once(__DIR__.'/../database/connection.db.php');
   require_once(__DIR__.'/../database/anuncios.class.php');
   require_once(__DIR__.'/../database/reviews.class.php');
   require_once(__DIR__.'/../database/users.class.php');
+  require_once(__DIR__.'/../templates/reviews.php');
 
-  $db = getDatabaseConnection();
-  $adId = isset($_GET['id']) ? intval($_GET['id']) : null;
-  $success = isset($_GET['success']) ? intval($_GET['success']) : 0;
-
-  if ($adId === null) {
-    die('Anúncio não encontrado.');
-  }
-
-  $ad = Ad::getById($db, $adId);
-
-  if (!$ad) {
-    die('Anúncio não encontrado.');
-  }
-
-  $reviews = Reviews::getByAdId($db, (int)$adId);
-  $averageRating = Reviews::getAverageRatingForAd($db, (int)$adId);
-  $reviewCount = Reviews::getReviewCountForAd($db, (int)$adId);
-
-  // Desenhar o layout
   drawHeader();
-  drawAdDetails($ad, $reviews,$averageRating, $reviewCount, $success);
+  drawAdDetails();
   drawFooter();
 ?>
