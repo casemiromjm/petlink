25f8771c4cf9ccb746a7d35644f9ece428f59050
casemiromjm <casemiromelojorge@gmail.com>
2025-05-25 17:25:46 +0100

    fixed profile update

--- templates/config.php
+++ templates/config.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1); ?>
 
-<?php require_once('../templates/sidebar.php'); ?>
+<?php require_once(__DIR__ . '/../templates/sidebar.php'); ?>
 
 <link rel="stylesheet" href="../stylesheets/style.css">
 <script src="../javascript/script.js" defer></script> 
